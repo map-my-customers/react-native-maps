0b17b22fb09f390649946b47d26f0dfb00cb35cb
Ryan Magoon <mr.magoon5@gmail.com>
2019-01-10 02:11:31 -0500

    add react-native 54 compatibility

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-import MapView from './lib/components/MapView';
+import MapView, {
+  Animated as ani,
+  ProviderPropType as prov,
+  MAP_TYPES as mapTypes
+} from './lib/components/MapView'
 
-export { default as Marker } from './lib/components/MapMarker.js';
-export { default as Polyline } from './lib/components/MapPolyline.js';
-export { default as Polygon } from './lib/components/MapPolygon.js';
-export { default as Circle } from './lib/components/MapCircle.js';
-export { default as UrlTile } from './lib/components/MapUrlTile.js';
-export { default as LocalTile } from './lib/components/MapLocalTile.js';
-export { default as Overlay } from './lib/components/MapOverlay.js';
-export { default as Callout } from './lib/components/MapCallout.js';
-export { default as Heatmap } from './lib/components/MapHeatmap.js';
-export { default as AnimatedRegion } from './lib/components/AnimatedRegion.js';
-export { Animated, ProviderPropType, MAP_TYPES } from './lib/components/MapView.js';
-export const PROVIDER_GOOGLE = MapView.PROVIDER_GOOGLE;
-export const PROVIDER_DEFAULT = MapView.PROVIDER_DEFAULT;
+export { default as Marker } from './lib/components/MapMarker.js'
+export { default as Polyline } from './lib/components/MapPolyline.js'
+export { default as Polygon } from './lib/components/MapPolygon.js'
+export { default as Circle } from './lib/components/MapCircle.js'
+export { default as UrlTile } from './lib/components/MapUrlTile.js'
+export { default as LocalTile } from './lib/components/MapLocalTile.js'
+export { default as Overlay } from './lib/components/MapOverlay.js'
+export { default as Callout } from './lib/components/MapCallout.js'
+export { default as Heatmap } from './lib/components/MapHeatmap.js'
+export { default as AnimatedRegion } from './lib/components/AnimatedRegion.js'
 
-export default MapView;
+export const PROVIDER_GOOGLE = MapView.PROVIDER_GOOGLE
+export const PROVIDER_DEFAULT = MapView.PROVIDER_DEFAULT
+
+export const Animated = ani
+export const ProviderPropType = prov
+export const MAP_TYPES = mapTypes
+
+export default MapView
